844c42207865b4ce1e4ca46d6b05e3adce19b483
Chris Gomes <gomezchris937@gmail.com>
2019-02-02 21:02:27 -0500

    upload

---
+++ app/data/friend.js
@@ -0,0 +1,167 @@
+
+
+var friends = [
+    {
+      name: "Peter Parker",
+      photo: "https://www.telegraph.co.uk/content/dam/gaming/2018/04/16/Spider-Man_PS4_Crouch_trans_NvBQzQNjv4Bqfmd-LgYrOTIHY1AkCfXGvLUKbdBQoK0iWiJG8T4idsY.jpg?imwidth=450",
+      scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "4",
+        "4",
+        "3",
+        "5",
+        "5",
+        "2"
+      ]
+    },
+    {
+      name: "Steve Rogers",
+      photo: "https://cdn3.movieweb.com/i/article/lLaIH7FR2SocKiB2YNLM1h9zVumGPM/798:50/Avengers-Infinity-War-Captain-America-Phone-Number-Easter.jpg",
+      scores: [
+        "5",
+        "5",
+        "3",
+        "4",
+        "3",
+        "5",
+        "5",
+        "1",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Tony Stark",
+      photo: "https://amp.thisisinsider.com/images/577d2929dd0895d15e8b45a1-750-441.jpg",
+      scores: [
+        "5",
+        "5",
+        "5",
+        "1",
+        "1",
+        "4",
+        "3",
+        "2",
+        "4",
+        "2"
+      ]
+    },
+    {
+      name: "James Buchanan",
+      photo: "https://womenwriteaboutcomics.com/wp-content/uploads/2016/05/78b0c03ffaaadfefc5077dc7e9cbe878.jpg",
+      scores: [
+        "4",
+        "2",
+        "3",
+        "2",
+        "1",
+        "4",
+        "2",
+        "3",
+        "2",
+        "4"
+      ]
+    },
+    {
+      name: "Thanos",
+      photo: "https://images.indianexpress.com/2019/01/avengers-thanos-snap-759-1.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "4",
+        "1",
+        "1",
+        "5",
+        "1",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Bruce Banner",
+      photo: "https://cdn.flickeringmyth.com/wp-content/uploads/2018/02/Captain-America-Civil-War-Hulk-Mark-Ruffalo-600x317.jpg",
+      scores: [
+        "4",
+        "1",
+        "3",
+        "5",
+        "1",
+        "5",
+        "3",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Dr.Strange",
+      photo: "https://www.sideshowtoy.com/assets/products/903595-doctor-strange/lg/marvel-avengers-infinifty-war-doctor-strange-sixth-scale-figure-hot-toys-903595-05.jpg",
+      scores: [
+        "4",
+        "5",
+        "3",
+        "1",
+        "1",
+        "3",
+        "3",
+        "2",
+        "5",
+        "3"
+      ]
+    },
+    {
+      name: "T Challa",
+      photo: "https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2018/03/black-panther-e1520897716634-822x675.jpg",
+      scores: [
+        "3",
+        "5",
+        "3",
+        "4",
+        "4",
+        "2",
+        "5",
+        "4",
+        "1",
+        "2"
+      ]
+    },
+    {
+      name: "Logan",
+      photo: "https://www.indiewire.com/wp-content/uploads/2017/02/10wrp4zrhzcnjka7dklcxz6ebhd.jpg?w=780",
+      scores: [
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "2"
+      ]
+    },
+    {
+      name: "Professor X",
+      photo: "http://image.guardian.co.uk/sys-images/Film/Pix/gallery/2003/04/29/xavier3.jpg",
+      scores: [
+        "4",
+        "1",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "3",
+        "5",
+        "5"
+      ]
+    },
+  ];
+  
+  module.exports = friends;
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,48 @@
+var friends= require("../data/friend");
+
+module.exports=function(app){
+
+app.get("/api/friends",function(req,res){
+    res.json(friends);
+});
+
+app.post("/api/friends",function(req,res){
+
+    var bestMatch={
+        name:"",
+        photo:"",
+        friendDifference:100
+    };
+
+    var userData=req.body;
+    var userScores=userData.scores;
+
+    var totalDifference;
+    // loop threw friends
+
+    for(var i=0;i<friends.length;i++){
+        var currentFriend=friends[i];
+        totalDifference=0;
+        console.log(currentFriend.name)
+            //loop threw scores araay index
+        for (var j=0;j<currentFriend.scores.length;j++){
+            // currentfriendscore = a\friend score array 
+            var currentFriendScore=currentFriend.scores[j];
+            // user input , whatever number they put in
+            var currentUserScore=userScores[j];
+            // makes sub from user- friendjs
+            totalDifference+=Math.abs(parseInt(currentUserScore)-parseInt(currentFriendScore));
+        }
+            // must be true if value is 0 it would break,,, score can not be higher then 100
+        if(totalDifference<=bestMatch.friendDifference){
+            bestMatch.name=currentFriend.name;
+            bestMatch.photo=currentFriend.photo;
+            bestMatch.friendDifference=totalDifference;
+
+        }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
+
+})
+}
